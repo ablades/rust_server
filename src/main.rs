4210418665c0a6838b24a5eb16b898b2ccd253f5
ablades <audaris@gmail.com>
2020-10-08 14:17:33 -0500

    complete connection handler

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
+
 
 fn main() {
     // create a listener 
@@ -14,8 +18,25 @@ fn main() {
 
 // Read and print data from the connection stream
 fn handle_connection(mut stream: TcpStream) {
-    let mut buff = [0; 1024];
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    stream.read(&mut buff).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buff[..]))
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
